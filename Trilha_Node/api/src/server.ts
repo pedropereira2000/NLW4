f7327b8af7219adbd0edc95ae95e51967ddbaf8c
Pedro Pereira <pedropereira.2000@alunos.utfpr.edu.br>
2021-02-23 12:49:30 -0300

    Segundo dia de NLW04

--- Trilha_Node/api/src/server.ts
+++ Trilha_Node/api/src/server.ts
@@ -1,30 +1,12 @@
-import express, { request, response } from 'express';
+import "reflect-metadata";
+import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- **/
-
-
-//http://localhost:3333/users
-
-app.get("/", (request, response) => {
-    //return response.send("Hello World - NLW04");
-    return response.json({ message: "Hello World - NLW05" });
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => Request, response
-
-app.post("/", (request, response) => {
-    //Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram salvos com sucesso!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
 
